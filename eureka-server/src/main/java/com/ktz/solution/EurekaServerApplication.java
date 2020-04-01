4e296ff93f86dd3f09ae897ee34415826a5dc694
kaituozhesh <kaituozhesh@hotmail.com>
2020-04-01 22:30:21 +0800

    Eureka初体验

---
+++ eureka-server/src/main/java/com/ktz/solution/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.ktz.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启用Eureka注册中心功能
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
